f2df98fc901dada91d330dce2055f49b98bc5498
ptullhub <111908049+ptullhub@users.noreply.github.com>
2024-08-31 05:49:15 -0400

    color
    You can change the color of the lines on the pieces by typing in a color, it can only use base unity colors because I tried using custom colors in the editor and it wouldn't display the lines

--- ToolsPluginsLab1/Assets/Scripts/DrawLine.cs
+++ ToolsPluginsLab1/Assets/Scripts/DrawLine.cs
@@ -3,22 +3,52 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class DrawLine : MonoBehaviour
 {
-    
     public GameObject[] GameObjects;
+    public string color = "green";
 
 #if UNITY_EDITOR
 
     void OnDrawGizmosSelected()
     {
-        Gizmos.color = Color.green;
-
-        for (int i = 0; i < GameObjects.Length; ++i)
+        switch (color)
         {
-            Gizmos.DrawLine(transform.position, GameObjects[i].transform.position);
+            case "green":
+                Gizmos.color = Color.green;
+                break;
+            case "white":
+                Gizmos.color = Color.white;
+                break;
+            case "red":
+                Gizmos.color = Color.red;
+                break;
+            case "blue":
+                Gizmos.color = Color.blue;
+                break;
+            case "black":
+                Gizmos.color = Color.black;
+                break;
+            case "yellow":
+                Gizmos.color = Color.yellow;
+                break;
+            default:
+                Gizmos.color = Color.green;
+                break;
         }
         
-    }
 
+        // Check if GameObjects array is not null
+        if (GameObjects != null && GameObjects.Length > 0)
+        {
+            for (int i = 0; i < GameObjects.Length; ++i)
+            {
+                // Ensure each GameObject in the array is not null
+                if (GameObjects[i] != null)
+                {
+                    Gizmos.DrawLine(transform.position, GameObjects[i].transform.position);
+                }
+            }
+        }
+    }
 
 #endif
 }
