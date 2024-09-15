13de602d4100c0c0c7978d6b1cdc2f668a54b441
ptullhub <111908049+ptullhub@users.noreply.github.com>
2024-09-14 23:04:12 -0400

    lab3

---
+++ ToolsPluginsLab3/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public Transform target;
+    private float speed = 20f;
+    private float rotationModifier = 90f;
+    private float rotationSpeed = 30f;
+    private float initialDistance;
+    private float adjustedSpeed;
+
+    private void Start()
+    {
+        if (target != null)
+        {
+            // Store the initial distance between the enemy and the player
+            initialDistance = Vector3.Distance(transform.position, target.position);
+
+            // Calculate adjusted rotation speed based on initial distance
+            adjustedSpeed = rotationSpeed * initialDistance;
+        }
+    }
+
+    void Update()
+    {
+        LookAtPlayer();
+        MaintainDistance();
+        RotateAroundPlayer();
+    }
+
+    private void LookAtPlayer()
+    {
+        if (target != null)
+        {
+            Vector3 vectorToTarget = target.position - transform.position;
+            float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - rotationModifier;
+            Quaternion quat = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, quat, Time.deltaTime * speed);
+        }
+    }
+
+    private void RotateAroundPlayer()
+    {
+        if (target != null)
+        {
+            // Compensate for player movement by rotating around the initial position plus the offset
+            transform.RotateAround(target.position, Vector3.forward, adjustedSpeed * Time.deltaTime);
+        }
+    }
+
+    private void MaintainDistance()
+    {
+        if (target != null)
+        {
+            Vector3 directionToTarget = (transform.position - target.position).normalized;
+            transform.position = target.position + directionToTarget * initialDistance;
+        }
+    }
+}
---
+++ ToolsPluginsLab3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    private float speed = 8f;
+    private float horizontal;
+
+    private void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+}
